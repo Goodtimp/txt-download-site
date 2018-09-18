001c5db53f985ad36231283c69ec3aa441ec0348
goodtimep <879636706@qq.com>
2018-09-18 16:30:04 +0800

    文件处理与域名小工具

--- HTTP/Request.js
+++ HTTP/Request.js
@@ -12,11 +12,12 @@ function GetUrlSrc(url){
 
     request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    return body; // Show the HTML for the baidu homepage.
+    console.log(body); // Show the HTML for the baidu homepage.
   }
 })
 }
 
+GetUrlSrc("www.baidu.com")
 
 module.exports={
     GetUrlSrc
