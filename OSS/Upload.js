24f9acdcecedc0b6772fa526789a731b7c63babd
tfh93121 <tfhhh@qq.com>
2018-09-18 14:09:26 +0800

    update upload.js

---
+++ OSS/Upload.js
@@ -0,0 +1,33 @@
+const OSS = require('ali-oss')
+const MD5 = require('md5')
+let client = new OSS({
+  region: '*****',
+  accessKeyId: '*****',
+  accessKeySecret: '*****',
+})
+client.useBucket('*****')
+
+
+function encryptMD5(text) {
+  return MD5(text)
+}
+
+/**
+ * 上传文件
+ * @param remoteName 远程文件名
+ * @param localName 本地路径
+ * @returns url 若上传成功，返回文件的远程路径，否则返回空字符串
+ */
+async function put(remoteName, localName) {
+  try {
+    let result = await client.put(remoteName, localName);
+    if(result.res.status == 200) return result.url
+  } catch (err) {
+    return ""
+  }
+}
+
+
+module.exports = {
+  put,
+}
